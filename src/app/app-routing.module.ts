dc4898055f3aaeaaaab9f5db2ad351f3cd8ce7a3
mendax02 <siddharth.rawat@outlook.com>
2020-06-13 23:25:03 +0530

    Resolving dynamic data resolve guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { AuthGuard } from './auth-guard.service';
 import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
 import { ErrorPageComponent } from './error-page/error-page.component';
+import { ServerResolver } from './servers/server/server-resolver.service';
 
  const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -20,8 +21,9 @@ import { ErrorPageComponent } from './error-page/error-page.component';
     // canActivate: [AuthGuard],
     canActivateChild: [AuthGuard],
     component: ServersComponent, children: [
+    { path: ':id', component: ServerComponent, resolve: {server: ServerResolver} },
     { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] },
-    { path: ':id', component: ServerComponent },
+
   ] },
  // {path: 'not-found', component: PageNotFoundComponent},
   {path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not found!'}},
