bbd8abbae60c16a8c1f08ebcc98550b5c0809edc
mendax02 <siddharth.rawat@outlook.com>
2020-06-13 22:52:58 +0530

    Passing static data to the Route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { ServerComponent } from './servers/server/server.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AuthGuard } from './auth-guard.service';
 import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
  const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -22,7 +23,8 @@ import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.s
     { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] },
     { path: ':id', component: ServerComponent },
   ] },
-  {path: 'not-found', component: PageNotFoundComponent},
+ // {path: 'not-found', component: PageNotFoundComponent},
+  {path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not found!'}},
   {path: '**', redirectTo: '/not-found', pathMatch: 'full'}
 
 ];
--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -34,6 +34,7 @@ export class EditServerComponent implements OnInit, CanComponentDeactivate {
     this.route.fragment.subscribe();
     const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
+    // this.route.params.subscribe();
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
